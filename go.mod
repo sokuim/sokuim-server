4b67aa3aae6d3a4f066540c18b024e33b6e72a7e
stifferchen831 <stifferchen831@gmail.com>
2026-08-17 00:14:51 +0800

    Init commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module sokuim/sokuim-server
+
+go 1.25
---
+++ main.go
@@ -0,0 +1,15 @@
+// Package main
+// File main.go
+// Copyright 2026 sokuim.com - All Rights Reserved
+// Link https://www.sokuim.com
+// Author stiffer.chen <stiffer@sokuim.com>
+// Created 2026-08-17 00:04:12
+// Modified 2026-08-17 00:04:12
+
+package main
+
+import "log"
+
+func main() {
+	log.Println("Welcome to sokuim.")
+}
