90758259e7716f472bcada005a626af2028cab84
stifferchen831 <stifferchen831@gmail.com>
2026-08-25 00:35:26 +0800

    job 001

--- app/job/job.go
+++ app/job/job.go
@@ -17,7 +17,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, Job: "+time.Now().String())
+		fmt.Fprintf(w, "Hello, Job-001: "+time.Now().String())
 	})
 	log.Println("Listening on :6073")
 	err := http.ListenAndServe(":6073", nil)
